8e54714cf7a9f3c32b220cc6abf31512753f24c4
carlentray <raynardcarlent24@gmail.com>
2025-06-24 18:50:38 +0700

    Update client.c

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 9999               
+#define PORT 8080
 #define BUFFER_SIZE 1024
 
 int main() {
@@ -14,42 +14,38 @@ int main() {
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
     char message[BUFFER_SIZE];
-
-    if ((sock = socket(9999, SOCK_STREAM, 0)) < 0) {
-        perror("socket");
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
         return -1;
     }
-
+    
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    if (inet_pton(AF_INET, "999.999.999.999", &serv_addr.sin_addr) <= 0) {
-        perror("inet_pton");
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connect");
         printf("\nConnection Failed \n");
         return -1;
     }
-
-    close(sock);
-
-    while (1) {
+    
+    while(1) {
         printf("Masukkan pesan untuk server: ");
         fgets(message, BUFFER_SIZE, stdin);
+
         message[strcspn(message, "\n")] = 0;
 
-        if (send(sock, message, strlen(message), 0) < 0) {
-            perror("send");
-        }
+        send(sock, message, strlen(message), 0);
 
         int valread = read(sock, buffer, BUFFER_SIZE);
         printf("Respons dari server: %s\n\n", buffer);
 
+        memset(buffer, 0, BUFFER_SIZE);
     }
-
+    
     return 0;
 }
